666f56133f8b7e96d7418e332007dfe14c980598
Arnatik76 <118046506+Arnatik76@users.noreply.github.com>
2024-12-12 15:15:24 +0500

    Changes

--- Mangaverse/src/main/java/org/example/mangaverse/controllers/Controller.java
+++ Mangaverse/src/main/java/org/example/mangaverse/controllers/Controller.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/read")
 public class Controller {
 
-    @GetMapping("/stalker_3")
-    public String index() {
-        return "index.html";
+    @GetMapping("/stalker_3/a")
+    public String stalker_3_a() {
+        return "read";
     }
 }
